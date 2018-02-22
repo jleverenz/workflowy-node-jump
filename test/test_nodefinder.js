462eeaee512b8cc0aa27e88d44ab4e796d979f13
Jeff Leverenz <jeff.leverenz@gmail.com>
2018-02-21 21:42:42 -0600

    Organize tests.

--- test/test_nodefinder.js
+++ test/test_nodefinder.js
@@ -2,53 +2,54 @@ var assert = require('assert');
 var nf = require('../src/nodefinder');
 
 describe('nodefinder', function() {
+    describe('customized lunr indexing', function() {
+        it('search nodes', function() {
+            var nodes = [
+                { id: 0, label: "caesar salad", depth: 3 },
+                { id: 1, label: "salad salad", depth: 2 },
+                { id: 2, label: "ice cream", depth: 2 },
+                { id: 3, label: "salad dressing", depth: 1 },
+                { id: 4, label: "greek salad", depth: 1 }
+            ];
 
-    var shallow_nodes = [
-        { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000001',
-          lm: 20686265,
-          nm: "Alpha"
-        },
-        { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000002',
-          lm: 20686265,
-          nm: "Beta"
-        }
-    ]
-
-    var child_nodes = [
-        { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000001',
-          lm: 20686265,
-          nm: "Alpha"
-        },
-        { ch: [{ id: 'aaaaaaaa-bbbb-cccc-dddd-000000000011',
-                 lm: 20686265,
-                 nm: "Child 1"
-               },
-               { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000012',
-                 lm: 20686265,
-                 nm: "Child 2"
-               }],
-          id: 'aaaaaaaa-bbbb-cccc-dddd-000000000002',
-          lm: 20686265,
-          nm: "Beta"
-        }
-    ]
-
-    var nodes = [
-        { id: 0, label: "caesar salad", depth: 3 },
-        { id: 1, label: "salad salad", depth: 2 },
-        { id: 2, label: "ice cream", depth: 2 },
-        { id: 3, label: "salad dressing", depth: 1 },
-        { id: 4, label: "greek salad", depth: 1 }
-    ];
-
-    it('search nodes', function() {
-        // compare function looks at value, which is shared for some items
-        nf.indexNodes(nodes);
-        var result = nf.search('salad');
-        assert.deepEqual([1,3,4,0], result.map(a => a.id));
+            // compare function looks at value, which is shared for some items
+            nf.indexNodes(nodes);
+            var result = nf.search('salad');
+            assert.deepEqual([1,3,4,0], result.map(a => a.id));
+        });
     });
 
     describe('processWorkflowyNodes', function() {
+        var shallow_nodes = [
+            { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000001',
+              lm: 20686265,
+              nm: "Alpha"
+            },
+            { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000002',
+              lm: 20686265,
+              nm: "Beta"
+            }
+        ]
+
+        var child_nodes = [
+            { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000001',
+              lm: 20686265,
+              nm: "Alpha"
+            },
+            { ch: [{ id: 'aaaaaaaa-bbbb-cccc-dddd-000000000011',
+                     lm: 20686265,
+                     nm: "Child 1"
+                   },
+                   { id: 'aaaaaaaa-bbbb-cccc-dddd-000000000012',
+                     lm: 20686265,
+                     nm: "Child 2"
+                   }],
+              id: 'aaaaaaaa-bbbb-cccc-dddd-000000000002',
+              lm: 20686265,
+              nm: "Beta"
+            }
+        ]
+
         it('returns list with labels and ids', function() {
             var result = nf.processWorkflowyNodes(shallow_nodes);
             assert.deepEqual(['Alpha', 'Beta'], result.map(a => a.label));
@@ -61,7 +62,6 @@ describe('nodefinder', function() {
     });
 
     describe('stableSort', function() {
-
         var docs;
 
         beforeEach(function() {
