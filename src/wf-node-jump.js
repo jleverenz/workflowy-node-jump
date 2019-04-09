6e2e75e3dcf591a74d58ccaaecf87b644f02986f
Jeff Leverenz <jeff.leverenz@gmail.com>
2019-04-09 01:37:34 -0500

    Bump get_initialization_data client_verion.

--- src/wf-node-jump.js
+++ src/wf-node-jump.js
@@ -33,7 +33,7 @@ function loadJQueryUI() {
  */
 function getWorkflowyData() {
   return new Promise((resolve, reject)=>{
-    $.get( "get_initialization_data?client_version=18", function( data ) {
+    $.get( "get_initialization_data?client_version=20", function( data ) {
       var rootChildren = data.projectTreeData.mainProjectTreeInfo.rootProjectChildren;
       resolve(nf.processWorkflowyNodes(rootChildren));
     });
